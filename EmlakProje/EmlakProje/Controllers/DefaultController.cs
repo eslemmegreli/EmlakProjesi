fd2f34e99bc0f6c36007efbe2062bfbb86094136
eslemmegreli <eslemmegreli@gmail.com>
2023-09-29 14:02:02 +0300

    EmlakProjesi
    MVC 5 ile hazırlanmıştır

---
+++ EmlakProje/EmlakProje/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmlakProje.Models.Entity;
+
+
+namespace EmlakProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        dbemlakEntities db = new dbemlakEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.tblEvler.ToList();
+            return View(degerler);
+        }
+    }
+}
